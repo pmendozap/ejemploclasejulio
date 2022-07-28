8b92c82a4be77846d11905238cb920cfa86e8942
Pablo Mendoza Ponce <pmendoza@itcr.ac.cr>
2022-07-28 14:59:24 -0600

    primer commit

---
+++ src/design/binario_bcd_v1.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 28.07.2022 14:53:54
+// Design Name: 
+// Module Name: binario_bcd_v1
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module binario_bcd_v1(
+
+    );
+endmodule
